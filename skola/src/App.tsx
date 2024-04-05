ecc1ebfabd613b9a158b8e5fd1642051bdfbd268
samadamp <94sampan@skola.boras.se>
2024-04-05 10:18:20 +0200

    Samrepo-componenter

---
+++ skola/src/App.tsx
@@ -0,0 +1,18 @@
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <NavBar/>
+    
+    
+    </>
+    
+  )
+}
+
+export default App
---
+++ skola/src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import logo from '../../public/img/kiwi.svg'
+import './NavBar.css';
+
+
+  
+const NavBar =  () => {
+    return (
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <h1>SAMS WORLD</h1>
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/portfolio">Portfolio</a></li>
+        </ul>
+      </nav>
+    );
+  };
+
+  export default NavBar
